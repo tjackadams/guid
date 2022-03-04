a5b790ca7cace501e25c78c46a42188fba441346
Thomas Adams <tjackadams@itadams.co.uk>
2022-03-04 15:12:29 +0000

    store guid options in local storage

--- GuidGen.Web/Program.cs
+++ GuidGen.Web/Program.cs
@@ -1,34 +1,29 @@
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
+using Blazored.LocalStorage;
 using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
+using GuidGen.Web;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace GuidGen.Web
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Logging.SetMinimumLevel(LogLevel.Debug);
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
-            builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services
-                .AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)})
-                .AddBlazorise(options => { options.ChangeTextOnKeyPress = true; })
-                .AddBootstrapProviders()
-                .AddFontAwesomeIcons();
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddBlazorise(options => { options.ChangeTextOnKeyPress = true; })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons()
+    .AddBlazoredLocalStorage();
 
-            var host = builder.Build();
+var host = builder.Build();
 
-            await host.RunAsync();
-        }
-    }
-}
+await host.RunAsync();
